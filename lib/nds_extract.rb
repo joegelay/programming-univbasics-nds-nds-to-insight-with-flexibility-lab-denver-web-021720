29a3851af9d0cd60b236d481b9248f5d78db8b51
Joe Gelay <joegelay@gmail.com>
2020-01-15 23:34:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,8 @@ def movies_with_director_key(name, movies_collection)
   movies = []
   i = 0 
   while i < movies_collection do 
-    movies << movie_with_director_name(name, )
+    movie_data = movies_collection[i]
+    movies << movie_with_director_name(name, movie_data)
     i += 1
   end
 
