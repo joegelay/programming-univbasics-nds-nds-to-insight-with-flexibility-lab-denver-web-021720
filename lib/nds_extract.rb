8ccf21ded36c3785f35f7004e00e7457bbe5f0b3
Joe Gelay <joegelay@gmail.com>
2020-01-16 00:04:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,7 +83,6 @@ def gross_per_studio(collection)
   return studio_totals
 end
 
-gross_per_studio(directors_database)
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
